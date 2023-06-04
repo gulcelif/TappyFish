ba107bcaafa291b2be9dcb5166e811296ba352a6
gulcelif <gulcelif3@gmail.com>
2023-06-04 14:46:21 +0300

    Restart Button
    Restart button and Game over panel added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static Vector2 bottomleft;
     public static bool gameOver;
-
+    public GameObject gameOverPanel;
     private void Awake()
     {
 
         bottomleft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+    }
+    public void RestartBtn()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     void Start()
     {
@@ -19,6 +24,7 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         gameOver = true;
+        gameOverPanel.SetActive(true); 
     }
     // Update is called once per frame
     void Update()
